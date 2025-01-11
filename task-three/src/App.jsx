1e672c1275dd14ab6ca6f67c20791b5ef74255d2
Satyajit Samal <samalsatyajit668@gmail.com>
2025-01-11 16:19:06 +0530

    implement form handling in App component; add input fields for user details and display submitted data

--- task-three/src/App.jsx
+++ task-three/src/App.jsx
@@ -1,35 +1,86 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const  [firstname, setFirstname]  = useState("");
+  const  [lastname, setLastname]  = useState("");
+  const  [email, setEmail]  = useState("");
+  const  [contact, setContact]  = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(firstname, lastname, email, contact);
+  };
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <div>Fill up the form below</div>
+        <div>
+          <form>
+            <label for="firstname">First Name: </label>
+            <input
+              type="text"
+              name="firstname"
+              id="firstname"
+              value={firstname}
+              placeholder="Enter your first name"
+              onChange={(e) => {
+                setFirstname(e.target.value);
+              }}
+            />
+            <label for="lastname">Last Name: </label>
+            <input
+              type="text"
+              name="lastname"
+              id="lastname"
+              value={lastname}
+              placeholder="Enter your last name"
+              onChange={(e) => {
+                setLastname(e.target.value);
+              }}
+            />
+            <label for="email">Email: </label>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              value={email}
+              placeholder="Enter your email id"
+              onChange={(e) => {
+                setEmail(e.target.value);
+              }}
+            />
+            <label for="contact">Contact:</label>
+            <input
+              type="text"
+              name="contact"
+              id="contact"
+              value={contact}
+              placeholder="Enter your contact"
+              onChange={(e) => {
+                setContact(e.target.value);
+              }}
+            />
+            <button
+              type="submit"
+              value="submit"
+              onClick={(e) => {
+                handleSubmit(e);
+              }}
+            >
+              Submit
+            </button>
+          </form>
+        </div>
+        <div>
+          <p>First Name: {firstname}</p>
+          <p>Last Name: {lastname}</p>
+          <p>Email: {email}</p>
+          <p>Contact: {contact}</p>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
