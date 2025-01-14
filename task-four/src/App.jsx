df87c3f82fb3ab53b7c2f83f64e118a9737cd15d
Satyajit Samal <samalsatyajit668@gmail.com>
2025-01-14 13:01:26 +0530

    Add Tailwind CSS integration and enhance styling in App component

--- task-four/src/App.jsx
+++ task-four/src/App.jsx
@@ -13,15 +13,16 @@ const App = () => {
   }
 
   return (
-    <>
-      <div>
-        <form onSubmit={handleSubmit(onSubmit)} >
-          <div>
+    <div className="bg-slate-900 h-screen flex justify-center items-center">
+      <div className="bg-slate-800 p-10 rounded-lg shadow-lg">
+        <form className="text-white text-3xl font-bold" onSubmit={handleSubmit(onSubmit)} >
+          <div className="">
             <label>Email: </label>
             <input
               type="email"
               name="email"
               placeholder="Enter your email here"
+              className="bg-slate-600 rounded-lg p-2 my-5 border border-slate-400"
               {...register("email", {
                 required: "Email is required",
                 pattern: {
@@ -38,6 +39,7 @@ const App = () => {
               type="password"
               name="password"
               placeholder="Enter your password here"
+              className="bg-slate-600 p-2 my-5 rounded-lg border border-slate-400"
               {...register("password", {
                 required: "Password is required",
                 minLength: {
@@ -48,10 +50,12 @@ const App = () => {
             /> 
             {errors.password && <p>{errors.password.message}</p>}
           </div>
-          <button type="submit">Login</button>
+          <div className="flex justify-center">
+          <button className="m-5 bg-gray-900 p-4 rounded-lg" type="submit">Login</button>
+          </div>
         </form>
       </div>
-    </>
+    </div>
   );
 };
 
