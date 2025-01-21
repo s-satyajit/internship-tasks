351a348d170a7b269a362029ea23d33ec2faf88f
Satyajit Samal <samalsatyajit668@gmail.com>
2025-01-21 06:13:27 +0530

    wip

---
+++ task-seven/src/components/Profile.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import UserContext from "../context/UserContext";
+import { useContext } from "react";
+
+const Profile = () => {
+
+    const {user} = useContext(UserContext)
+
+    if(!user) return <div>Please Login</div>
+
+    return (
+        <div>Welcome {user.username}</div>
+    )
+}
+
+export default Profile;
