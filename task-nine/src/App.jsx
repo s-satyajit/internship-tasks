9b91e107683c132ef2534218703322df45230450
Satyajit Samal <samalsatyajit668@gmail.com>
2025-01-23 17:14:45 +0530

    wip

---
+++ task-nine/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import LazyLoading from './components/LazyLoading'
+
+function App() {
+
+  return (
+    <>
+      <h1>Lazy Loading</h1>
+      <LazyLoading />
+    </>
+  )
+}
+
+export default App
