4ba82ab26d08d66f12d1a4c168e0388a770bd8fd
Satyajit Samal <samalsatyajit668@gmail.com>
2025-01-25 12:38:08 +0530

    refactor

--- task-four/src/components/EmailPage.jsx
+++ task-four/src/components/EmailPage.jsx
@@ -19,14 +19,19 @@ const EmailPage = ({ setIsLoggedIn }) => {
 
   return (
     <div className="h-screen bg-slate-900 flex justify-center items-center text-white">
-      <form className="bg-slate-800 shadow-2xl p-10 rounded-lg" onSubmit={handleSubmit(onSubmit)}>
+      <form
+        className="bg-slate-800 shadow-2xl p-10 rounded-lg"
+        onSubmit={handleSubmit(onSubmit)}
+      >
         <div className="my-4">
           <label className="block mb-2">Email:</label>
           <input
             type="email"
             name="email"
             placeholder="Enter your email here"
-            className={`bg-slate-600 p-2 rounded-lg border ${errors.email ? 'border-red-500' : 'border-slate-500'}`}
+            className={`bg-slate-600 p-2 rounded-lg border ${
+              errors.email ? "border-red-500" : "border-slate-500"
+            }`}
             {...register("email", {
               required: "Email is required",
               pattern: {
@@ -35,7 +40,9 @@ const EmailPage = ({ setIsLoggedIn }) => {
               },
             })}
           />
-          {errors.email && <span className="text-red-500">{errors.email.message}</span>}
+          {errors.email && (
+            <span className="text-red-500">{errors.email.message}</span>
+          )}
         </div>
         <div className="my-4">
           <label className="block mb-2">Password:</label>
@@ -43,14 +50,21 @@ const EmailPage = ({ setIsLoggedIn }) => {
             type="password"
             name="password"
             placeholder="Enter your password here"
-            className={`bg-slate-600 p-2 rounded-lg border ${errors.password ? 'border-red-500' : 'border-slate-500'}`}
+            className={`bg-slate-600 p-2 rounded-lg border ${
+              errors.password ? "border-red-500" : "border-slate-500"
+            }`}
             {...register("password", {
               required: "Password is required",
             })}
           />
-          {errors.password && <span className="text-red-500">{errors.password.message}</span>}
+          {errors.password && (
+            <span className="text-red-500">{errors.password.message}</span>
+          )}
         </div>
-        <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+        <button
+          type="submit"
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        >
           Login
         </button>
       </form>
